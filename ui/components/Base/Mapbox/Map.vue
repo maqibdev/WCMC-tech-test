0a9d9a11234a072010f5dcdbf74faab8ce485d5b
maqibdev <maqib.dev@gmail.com>
2024-11-20 13:59:03 +0000

    Added ArcGIS soruce dataset and added a layer to show markers

--- ui/components/Base/Mapbox/Map.vue
+++ ui/components/Base/Mapbox/Map.vue
@@ -40,6 +40,23 @@ export default {
 
     this.map.on('load', () => {
       this.loaded = true
+      this.map.addSource('arcgis-data', {
+        type: 'geojson',
+        data: 'https://services3.arcgis.com/fp1tibNcN9mbExhG/arcgis/rest/services/Fiji_Sponges_Algae/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
+      })
+
+      this.map.addLayer({
+        id: 'markers',
+        source: 'arcgis-data',
+        type: 'circle',
+        filter: ['!', ['has', 'point_count']],
+        paint: {
+          'circle-color': '#8a11da',
+          'circle-radius': 5,
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#fff'
+        }
+      })
     })
   }
 }
