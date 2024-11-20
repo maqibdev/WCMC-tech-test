1746b54a2a987b363946cf0e73cae52e8baa1f49
maqibdev <maqib.dev@gmail.com>
2024-11-20 18:37:40 +0000

    Added Clustering based on proximity, also increase the map size to from 512px to 712px

--- ui/components/Base/Mapbox/Map.vue
+++ ui/components/Base/Mapbox/Map.vue
@@ -32,6 +32,141 @@ export default {
     }
   },
 
+  mounted () {
+    this.map = new Mapbox.Map({
+      container: this.$refs.map,
+      ...this.configuration
+    })
+
+    this.map.on('load', () => {
+      this.loaded = true
+      this.map.addSource('arcgis-data', {
+        type: 'geojson',
+        data: 'https://services3.arcgis.com/fp1tibNcN9mbExhG/arcgis/rest/services/Fiji_Sponges_Algae/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson',
+        cluster: true,
+        clusterMaxZoom: 14,
+        clusterRadius: 50
+      })
+
+      this.map.addLayer({
+        id: 'clusters',
+        source: 'arcgis-data',
+        type: 'circle',
+        filter: ['has', 'point_count'],
+        paint: {
+          'circle-color': [
+            'step',
+            ['get', 'point_count'],
+            '#51bbd6',
+            100,
+            '#f1f075',
+            750,
+            '#f28cb1'
+          ],
+          'circle-radius': [
+            'step',
+            ['get', 'point_count'],
+            20,
+            100,
+            30,
+            750,
+            40
+          ]
+        }
+      })
+
+      this.map.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'arcgis-data',
+        filter: ['has', 'point_count'],
+        layout: {
+          'text-field': ['get', 'point_count_abbreviated'],
+          'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+          'text-size': 12
+        }
+      })
+
+      this.map.addLayer({
+        id: 'markers',
+        source: 'arcgis-data',
+        type: 'circle',
+        filter: ['!', ['has', 'point_count']],
+        paint: {
+          'circle-color': '#8a11da',
+          'circle-radius': 5,
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#fff'
+        }
+      })
+
+      this.map.on('click', 'clusters', (e) => {
+        const features = this.map.queryRenderedFeatures(e.point, {
+          layers: ['clusters']
+        })
+        const clusterId = features[0].properties.cluster_id
+
+        this.map.getSource('arcgis-data').getClusterExpansionZoom(
+          clusterId,
+          (err, zoom) => {
+            if (err) { return }
+            this.map.easeTo({
+              center: features[0].geometry.coordinates,
+              zoom
+            })
+          }
+        )
+      })
+
+      this.map.on('click', 'markers', (e) => {
+        const features = this.map.queryRenderedFeatures(e.point, {
+          layers: ['markers']
+        })
+
+        const uniqueFeatures = Array.from(
+          new Map(features.map(feature => [feature.properties.id, feature])).values()
+        )
+
+        const speciesList = uniqueFeatures.map(feature => ({
+          id: feature.properties.id,
+          scientificName: feature.properties.scientificName,
+          genus: feature.properties.genus,
+          family: feature.properties.family,
+          country: feature.properties.country,
+          waterBody: feature.properties.waterBody,
+          scientificNameAuthorship: feature.properties.scientificNameAuthorship
+        }))
+        const locationName = features[0].properties.locality || 'Unknown Location'
+
+        let currentPage = 1
+
+        const popup = new Mapbox.Popup()
+          .setLngLat(features[0].geometry.coordinates.slice())
+          .setHTML(this.generateTooltip(speciesList, locationName, currentPage))
+          .addTo(this.map)
+
+        popup.getElement().addEventListener('click', (event) => {
+          if (event.target.classList.contains('prev')) {
+            currentPage -= 1
+          } else if (event.target.classList.contains('next')) {
+            currentPage += 1
+          } else {
+            return
+          }
+
+          popup.setHTML(this.generateTooltip(speciesList, locationName, currentPage))
+        })
+      })
+
+      this.map.on('mouseenter', 'clusters', () => {
+        this.map.getCanvas().style.cursor = 'pointer'
+      })
+      this.map.on('mouseleave', 'clusters', () => {
+        this.map.getCanvas().style.cursor = ''
+      })
+    })
+  },
+
   methods: {
     generateTooltip (speciesList, locationName, currentPage = 1) {
       const totalPages = speciesList.length
@@ -62,75 +197,13 @@ export default {
         ${paginationHtml}
       `
     }
-  },
-
-  mounted () {
-    this.map = new Mapbox.Map({
-      container: this.$refs.map,
-      ...this.configuration
-    })
-
-    this.map.on('load', () => {
-      this.loaded = true
-      this.map.addSource('arcgis-data', {
-        type: 'geojson',
-        data: 'https://services3.arcgis.com/fp1tibNcN9mbExhG/arcgis/rest/services/Fiji_Sponges_Algae/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
-      })
-
-      this.map.addLayer({
-        id: 'markers',
-        source: 'arcgis-data',
-        type: 'circle',
-        filter: ['!', ['has', 'point_count']],
-        paint: {
-          'circle-color': '#8a11da',
-          'circle-radius': 5,
-          'circle-stroke-width': 1,
-          'circle-stroke-color': '#fff'
-        }
-      })
-
-      this.map.on('click', 'markers', (e) => {
-        const features = this.map.queryRenderedFeatures(e.point, { layers: ['markers'] })
-
-        const speciesList = features.map(feature => ({
-          id: feature.properties.id,
-          scientificName: feature.properties.scientificName,
-          genus: feature.properties.genus,
-          family: feature.properties.family,
-          country: feature.properties.country,
-          waterBody: feature.properties.waterBody,
-          scientificNameAuthorship: feature.properties.scientificNameAuthorship
-        }))
-        const locationName = features[0].properties.locality || 'Unknown Location'
-
-        let currentPage = 1
-
-        const popup = new Mapbox.Popup()
-          .setLngLat(features[0].geometry.coordinates.slice())
-          .setHTML(this.generateTooltip(speciesList, locationName, currentPage))
-          .addTo(this.map)
-
-        popup.getElement().addEventListener('click', (event) => {
-          if (event.target.classList.contains('prev')) {
-            currentPage -= 1
-          } else if (event.target.classList.contains('next')) {
-            currentPage += 1
-          } else {
-            return
-          }
-
-          popup.setHTML(this.generateTooltip(speciesList, locationName, currentPage))
-        })
-      })
-    })
   }
 }
 </script>
 
 <style lang="postcss" scoped>
 .ct-map {
-  height: 512px;
+  height: 712px;
 
   &__container {
     @apply h-full;
